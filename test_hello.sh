5e338398eb69325fc4137ff619e90b22412b108b
WeiDe Su <weide_su@mitake.com.tw>
2018-09-10 14:20:33 +0800

    Update script which use case and function for selecting target

--- test_hello.sh
+++ test_hello.sh
@@ -2,11 +2,35 @@
 
 H_CONTENT_TYPE=content-type:application/json;charset=UTF-8
 BODY='{"code":0,"content":"TEST"}'
-#HOST=lit-springs-63926.herokuapp.com
-HOST=127.0.0.1
+HOST_HEROKU=lit-springs-63926.herokuapp.com
+HOST_LOCAL=127.0.0.1
 
-echo ${BODY}
+# Send curl request to target-host
+function sendToLocal() {
+  curl -v -H ${H_CONTENT_TYPE}  --data ${BODY} -X POST http://${TARGET_HOST}:8080/hello
+}
 
-curl -v -H ${H_CONTENT_TYPE}  --data ${BODY} -X POST http://${HOST}:8080/hello
 
-#curl -v -H ${H_CONTENT_TYPE}  --data ${BODY} -X POST https://${HOST}/hello
+function sendToHeroku() {
+  curl -v -H ${H_CONTENT_TYPE}  --data ${BODY} -X POST https://${TARGET_HOST}/hello
+}
+
+OPTION=$1
+case $1 in
+  1)
+    TARGET_HOST=${HOST_LOCAL}
+    echo "Send to Localhost ${HOST_LOCAL}"
+    sendToLocal;
+  ;;
+
+  2)
+    TARGET_HOST=${HOST_HEROKU}
+    echo "Send to Heroku ${HOST_HEROKU}"
+    sendToHeroku;
+  ;;
+
+  *)
+    echo "Usage $0 1(Localhost), 2(Hekoru)"
+  ;;
+esac
+
