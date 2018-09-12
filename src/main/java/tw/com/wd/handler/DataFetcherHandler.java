47670182e00976d13a0eb2840631f9f9f3e47e3a
WeiDe Su <weide_su@mitake.com.tw>
2018-09-12 17:34:51 +0800

    Fix bug that use null value as URL

--- src/main/java/tw/com/wd/handler/DataFetcherHandler.java
+++ src/main/java/tw/com/wd/handler/DataFetcherHandler.java
@@ -7,17 +7,28 @@ import tw.com.wd.obj.FireAlertObj;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 public class DataFetcherHandler extends AbstractFireAlertHandler {
+    private URL fireAlertURL;
+
+
+    public DataFetcherHandler() {
+        super();
+        try {
+            this.fireAlertURL = new URL("http://epaper.fire.ntpc.gov.tw/liveview/default.aspx");
+        } catch (MalformedURLException e) {
+            throw new RuntimeFireAlertException(e);
+        }
+    }
+
     @Override
     protected void processing(FireAlertObj fireAlertObj) throws RuntimeFireAlertException {
-        URL dataURL = fireAlertObj.getData(FireAlertObj.KEY_DATA_URL);
-
         try {
             ClassLoader classLoader = getClass().getClassLoader();
             File file = new File(classLoader.getResource("fire_alert.html").getFile());
-            Document doc = Jsoup.parse(dataURL, 3000);
+            Document doc = Jsoup.parse(this.fireAlertURL, 3000);
             //Document doc = Jsoup.parse(file, "UTF-8");
 
             fireAlertObj.putData(FireAlertObj.KEY_DATA_DOC, doc);
